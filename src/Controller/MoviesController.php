e6aaf241f011e7889a02b3e47afde2ab1957cbd5
Roman Grubic <grubic.roman@gmail.com>
2022-05-03 13:42:53 +0200

    Basics of Symfony

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    /**
+     * @Route("/movies", name="movies")
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', [
+            'title' => 'Avengers'
+        ]);
+    }
+}
