94b1e6e77f82a1d31dc8ddec23c8ebad8d06a055
Roman Grubic <grubic.roman@gmail.com>
2022-05-04 13:35:33 +0200

    Trying out Symfony DB functions.

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,21 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * @Route("/movies", name="movies")
      */
     public function index(): Response
     {
-        $movies = ["Avengers", "Inception", "Loki", "Sonic 2"];
+        // findAll() -> Select * from movies;
+        // find(5) - Select * from movies where id = 5;
 
-        return $this->render('index.html.twig', array(
-            "movies" => $movies
-        ));
+
+        // findBy() = select * from movies ORDER by id DESC
+
+        // $movies = $repository->findOneBy(['id' => 5, 'title' => 'The Dark Knight'],['id' => 'DESC']);
+        // findOneBy = select * from movies where id = 5 and title = 'The Dark Knight' ORDER by DESC
+
+        // $movies = $repository->count(['id' => 5]);
+        // count() = select count() from movies where id = 5
+
+        $repository = $this->em->getRepository(Movie::class);
+        $movies = $repository->findByReleaseYear(2019);
+
+        dd($movies);
+
+        return $this->render('index.html.twig');
     }
 }
---
+++ src/DataFixtures/MovieFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Movie;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class MovieFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $movie = new Movie();
+        $movie->setTitle('The Dark Knight');
+        $movie->setReleaseYear(2008);
+        $movie->setDescription('Description of The Dark Knight');
+        $movie->setImagePath('https://cdn.pixabay.com/photo/2021/06/18/11/22/batman-6345897_960_720.jpg');
+
+        // Add data to pivot table
+        $movie->addActor($this->getReference('actor_1'));
+        $movie->addActor($this->getReference('actor_2'));
+        $manager->persist($movie);
+
+        $movie2 = new Movie();
+        $movie2->setTitle('Avengers: Endgame');
+        $movie2->setReleaseYear(2019);
+        $movie2->setDescription('Description of Avengers: Endgame');
+        $movie2->setImagePath('https://pixabay.com/illustrations/captain-america-avengers-marvel-5692937/');
+        // Add data to pivot table
+        $movie2->addActor($this->getReference('actor_3'));
+        $movie2->addActor($this->getReference('actor_4'));
+        $manager->persist($movie2);
+
+        $manager->flush();
+    }
+}
